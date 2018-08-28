34fb8398e9f2ac1030d9cc5209bef48df856ace4
João Ribeiro <joaosoft@gmail.com>
2018-08-28 20:22:29 +0100

    fix

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	logger "github.com/joaosoft/logger"
+)
 
 func main() {
 	fmt.Println(NewSampleTestTwo())
 }
 
 func NewSampleTestTwo() string {
+	logger.Info("executing info logger for sample-test-two")
 	return "hello, i'm the sample-test-two"
 }
